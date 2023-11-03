74752d52b9797a715a5e202733f897c3290f558d
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-03 19:24:37 +0100

    finished task-01

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,9 @@
-const categories = document.querySelectorAll("li.item");
-console.log(`Number of categories: ${ categories.length }`);
+const listItem = document.querySelectorAll("li.item");
+console.log(`Number of categories: ${listItem.length}`);
+listItem.forEach(categorie=>{
+    const categorieName = categorie.querySelector("h2")
+    console.log(`Category: ${categorieName.textContent}`)
+    const categorieList = categorie.querySelectorAll("li")
+    console.log(`Elements: ${categorieList.length}` );
+    }
+)
