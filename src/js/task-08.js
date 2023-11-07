32da659e8e68294d24a3a176f08ba410153e7dac
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-07 14:54:54 +0100

    Complete task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector("form.login-form");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+   
+    const info = {
+        email: email.value,
+        password: password.value 
+    }
+    if ((email.value === "" || password.value === "")|| (email.value === " "|| password.value === " ")) {
+        alert("Please, complete the form");
+    } else {
+        console.log(info);
+        form.reset()
+    }
 
+}
