618d4f52880e9161e5066e920bc996a6654c77fa
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-07 15:06:21 +0100

    Complete task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const btnChangeColor = document.querySelector("button.change-color");
+const colorName = document.querySelector("span.color");
+
+btnChangeColor.addEventListener("click", handleClick);
+
+function handleClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
+}
