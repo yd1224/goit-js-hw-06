9ce1b3bea8c4e2b41845aeac985d3b5e59658660
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-08 15:53:35 +0100

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,8 +1,9 @@
 const input = document.querySelector("input#font-size-control");
 const text = document.querySelector("span#text")
+text.style.fontSize = input.value + "px";
 input.addEventListener("input", handleInput)
 
 function handleInput(event) {
     const inputValue = event.currentTarget.value;
-    text.style.fontSize = inputValue + "px";
+ text.style.fontSize = inputValue + "px";
 }
