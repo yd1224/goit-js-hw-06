0d88eaf79989ab53bf8ad245b24e59720f4869eb
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-08 15:47:02 +0100

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,11 +1,17 @@
 const input = document.querySelector("#validation-input");
-const maxLength = input.getAttribute("data-length");
+const length = input.getAttribute("data-length");
 input.addEventListener("blur", handleBlur);
 function handleBlur(event) {
    const inputValue = event.currentTarget.value;
-    const isValid = inputValue.length <= maxLength;
+    const isValid = inputValue.length === Number(length);
+
 
     // Toggle "valid" or "invalid" classes based on the validity.
-    input.classList.toggle("valid", isValid);
-    input.classList.toggle("invalid", !isValid);
+
+        input.classList.toggle("valid", isValid); 
+
+         input.classList.toggle("invalid", !isValid);
+
+   
+   
 }
